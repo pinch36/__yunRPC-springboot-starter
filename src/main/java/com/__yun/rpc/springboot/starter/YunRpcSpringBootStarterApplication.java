08edd6fd2f3444910f490e1e232eb86abec33dd5
__yun <ylw167@qq.com>
2024-09-18 10:54:30 +0800

    注解驱动开发

--- src/main/java/com/__yun/rpc/springboot/starter/YunRpcSpringBootStarterApplication.java
+++ src/main/java/com/__yun/rpc/springboot/starter/YunRpcSpringBootStarterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableRpc
 public class YunRpcSpringBootStarterApplication {
 
     public static void main(String[] args) {
